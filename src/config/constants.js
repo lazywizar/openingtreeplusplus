3f8bd1b47ab59f0cac890af5f8e8611455f31ae6
lazywizar <bit.varun@gmail.com>
2024-11-25 17:31:23 -0800

    Moving config to .env file

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,7 @@
-export const REPERTOIRE_CONFIG = {
-    MAX_MOVES_TO_COMPARE: 10
+require('dotenv').config();
+
+const MAX_MOVES_TO_COMPARE = process.env.MAX_MOVES_TO_COMPARE || 10; // Default to 10 if not set
+
+module.exports = {
+    MAX_MOVES_TO_COMPARE
 };
